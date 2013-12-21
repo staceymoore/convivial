c29d1ca9361f9799229fa4b9995fd29dce955707
Stacey Moore <staceymoore@users.noreply.github.com>
2013-12-21 13:16:46 -0800

    Added version number for tagged release

--- jquery.convivial.js
+++ jquery.convivial.js
@@ -1,6 +1,6 @@
 /*!
  *  Convivial https://github.com/staceymoore/convivial
- *  Version: v0.11-beta
+ *  Version: 0.1.0
  *  Author: Stacey Moore
  *  License: MIT 
  *	Description: A jQuery social sharing plugin based on Sharrre by Julien Hany (http://sharrre.com/) 
